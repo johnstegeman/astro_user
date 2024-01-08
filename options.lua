3faacabde1cae252bb43e4e5a2eb9c64c7537377
John Stegeman <john.stegeman@gmail.com>
2024-01-08 12:59:09 -0500

    Added chezmoi-ified version of .zshrc to shell config for LSP and treesitter

--- options.lua
+++ options.lua
@@ -35,6 +35,7 @@
 	filename = {
 		[".zshrc"] = "sh",
 		[".zshenv"] = "sh",
+		["dot_zshenv"] = "sh",
 	},
 }
 --   local_vim.opt.whichwrap = vim.opt.whichwrap - { 'b', 's' } -- removing option from list
